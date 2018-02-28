1b41fce6c48b1f09cca36b07d741af5b15a1c7c5
Shawn Neal <sneal@sneal.net>
2018-02-28 11:33:49 -0800

    Rename opsman host to follow typical naming convention

--- ops_manager/dns.tf
+++ ops_manager/dns.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "ops_manager" {
-  name    = "pcf.${var.env_name}.${var.dns_suffix}"
+  name    = "opsman.${var.env_name}.${var.dns_suffix}"
   zone_id = "${var.zone_id}"
   type    = "A"
   ttl     = 300
